df0c0b100643d2d69e03fa68457f953dcddb3cc2
Anton Kulchytskyi <antek1809@gmail.com>
2024-09-30 11:07:10 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as styles from '../src/styles/App.module.css';
+
+const App = () => {
+  return <h1 className={styles.title}>hello from react with TS and webpack</h1>;
+};
+
+export default App;
---
+++ webpack.config.cjs
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+    assetModuleFilename: 'images/[name][ext]',
+  },
+  devServer: {
+    static: path.join(__dirname, '/dist'),
+
+    port: 4444,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/, type: 'asset/resource' },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+};
