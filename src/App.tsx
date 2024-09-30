9bbc58984dd0842ce509843f8fc3753b07484f74
Anton Kulchytskyi <antek1809@gmail.com>
2024-09-30 17:14:10 +0300

    added simple fetch for check deploy and connect to the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react';
 import * as styles from '../src/styles/App.module.css';
 
 const App = () => {
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    // fetch('https://family-tree-task-server.onrender.com/familymembers')
+    fetch('http://localhost:3000/familymembers')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPhotos(data);
+      });
+  }, []);
   return <h1 className={styles.title}>hello from react with TS and webpack</h1>;
 };
 
